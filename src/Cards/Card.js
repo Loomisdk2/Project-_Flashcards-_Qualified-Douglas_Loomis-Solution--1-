900732a690b7e0d8428a1c75e18d59c7f95c4c82
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-20 15:55:24 -0500

    Worked on src/Cards/Card

---
+++ src/Cards/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Card({ card, count, index, flipped, flip, next }) {
+  const nextButton = (
+    <button type="button" className="btn btn-primary" onClick={next}>
+      next
+    </button>
+  );
+
+  return (
+    <>
+      <div className="card">
+        <div className="card-body">
+          <h4 className="card-title">
+            Card {index} of {count}
+          </h4>
+          <p className="card-text">{!flipped ? card.front : card.back}</p>
+          <button
+            type="button"
+            className="btn btn-secondary mr-2"
+            onClick={flip}
+          >
+            Flip
+          </button>
+          {flipped && nextButton}
+        </div>
+      </div>
+    </>
+  );
+}
