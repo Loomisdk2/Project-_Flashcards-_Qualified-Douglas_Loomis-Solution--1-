139d2c11c0824ef9110bc1f07097de456c05e268
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-24 14:14:07 -0500

    Imported icons in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./Layout";
 import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 /**
  * App is a wrapper for <Layout>, you should not need to change this file.
