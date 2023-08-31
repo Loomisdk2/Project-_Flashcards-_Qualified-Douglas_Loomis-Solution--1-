3f1a5139de2ecf827f678115cf3615ac1c7ec321
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-31 15:40:08 -0500

    Added comments to Deck/DeckList

--- src/Deck/DeckList.js
+++ src/Deck/DeckList.js
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 import { deleteDeck } from "../utils/api/index.js";
 
 export default function Decklist({ decks }) {
+  // Handle delete function
   async function handleDelete(id) {
     const abortCon = new AbortController();
     try {
+      // Window to confirm deletion
       const result = window.confirm(
         "Delete this deck?\n\n\nYou will not be able to recover it."
       );
+      // If user confirms deletion, then...
       if (result) {
         await deleteDeck(id, abortCon.signal);
+        // reload page
         window.location.reload();
       }
+      // throw any errors
     } catch (err) {
       throw err;
     }
+    // Cleanup function
     return () => abortCon.abort();
   }
 
@@ -34,7 +40,7 @@ export default function Decklist({ decks }) {
                 </p>
               </div>
             </div>
-            <p className="card-text">{deck.description}</p>
+            <p className="card-info">{deck.description}</p>
             <div className="d-flex justify-content-between">
               <div className="">
                 <Link
@@ -56,7 +62,7 @@ export default function Decklist({ decks }) {
                   type="button"
                   onClick={() => handleDelete(deck.id)}
                 >
-                  <i className="fas fa-trash-can"></i>
+                  <i className="fas fa-trash-alt mr-1"></i>
                 </button>
               </div>
             </div>
