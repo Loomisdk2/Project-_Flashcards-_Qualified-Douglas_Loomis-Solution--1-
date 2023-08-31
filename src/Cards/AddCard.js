41a6d7b8966d5abf449bbfae199d28f81f8d1d3d
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-31 16:52:58 -0500

    Comments added to Cards/AddCards

--- src/Cards/AddCard.js
+++ src/Cards/AddCard.js
@@ -1,26 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { readDeck } from "../utils/api";
 import NavBar from "../Layout/NavBar";
 import CardForm from "./CardForm";
-import { readDeck } from "../utils/api";
 
 export default function AddCard() {
-  const { deckId } = useParams();
+  // State hook for current deck
   const [currentDeck, setCurrentDeck] = useState({});
+  const { deckId } = useParams();
 
   useEffect(() => {
+    // Abort controller created
     const abortCon = new AbortController();
     async function getDeck() {
       try {
+        // Check if deckId exists
         if (deckId) {
           const gotDeck = await readDeck(deckId, abortCon.signal);
+          // Update the currentDeck with the data from haveDeck
           setCurrentDeck({ ...gotDeck });
         }
+        // Throw any errors
       } catch (err) {
         throw err;
       }
     }
+    // Calls getDeck function
     getDeck();
+    // Returns a cleanup function
     return () => abortCon.abort();
   }, [deckId]);
 
