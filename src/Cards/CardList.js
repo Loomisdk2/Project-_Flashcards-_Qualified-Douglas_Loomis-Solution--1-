c5d05da554533fbc991419a1b1515da28281f0d5
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-31 16:34:55 -0500

    Comments added to Cards/CardList

--- src/Cards/CardList.js
+++ src/Cards/CardList.js
@@ -3,23 +3,30 @@ import { useHistory, useRouteMatch } from "react-router-dom";
 import { deleteCard } from "../utils/api";
 
 export default function CardList({ cards }) {
+  // Get browesers history object and URL
   const history = useHistory();
   const { url } = useRouteMatch();
 
+  // Handle delete card
   async function handleDelete(id) {
+    // Abort controller for handling abort signals
     const abortCon = new AbortController();
     try {
+      // Display confirmation text to user
       const result = window.confirm(
         "Delete this card?\n\n\nYou will not be able to recover it."
       );
+      // If user confirms deletion, then...
       if (result) {
+        // Call deletCard API function with card id and the abort signal
         await deleteCard(id, abortCon.signal);
+        // Reload window to show updated card list
         window.location.reload();
       }
+      // Throw error if there are any
     } catch (err) {
       throw err;
     }
-    return () => abortCon.abort();
   }
 
   return (
