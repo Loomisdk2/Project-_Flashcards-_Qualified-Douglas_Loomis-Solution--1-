6a04d9717b6e6340765e580738fbdc8569dfd427
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-31 15:33:04 -0500

    Comments added and corrected line 32 Deck/EditDeck

--- src/Deck/EditDeck.js
+++ src/Deck/EditDeck.js
@@ -5,22 +5,33 @@ import { readDeck } from "../utils/api";
 import DeckForm from "./DeckForm";
 
 export default function EditDeck() {
+  // State to store current deck data
   const [deck, setDeck] = useState({});
-
+  // Extract deckId parameter form the URL
   const { deckId } = useParams();
 
+  // Fetch and set current deck data
   useEffect(() => {
+    // Abort controller for cleanup
     const abortCon = new AbortController();
+    // Fetch current deck data
     async function getCurrentDeck() {
       try {
+        // Fetch deck data using readDeck function from API
         const currentDeck = await readDeck(deckId, abortCon.signal);
         setDeck({ ...currentDeck });
+        // Catch and throw any errors
       } catch (err) {
         throw err;
       }
     }
+    // Call function to fetch current deck data
     getCurrentDeck();
-    return abortCon.abort();
+
+    // Cleanup function
+    return () => {
+      abortCon.abort();
+    };
   }, [deckId]);
 
   return (
