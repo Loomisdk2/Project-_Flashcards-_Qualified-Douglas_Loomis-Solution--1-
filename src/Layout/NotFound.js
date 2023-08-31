bd78cdf9286df6489351f0e1bca8a8f86bfe8c78
Douglas Loomis <125160013+Loomisdk2@users.noreply.github.com>
2023-08-31 17:00:33 -0500

    Touched up Layout/NotFound

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function NotFound() {
+function NotFound({ backHome }) {
   return (
-    <>
+    <div className="NotFound">
       <h2>Not Found</h2>
       <Link to={"/"} className="btn btn-warning">
         Return Home
       </Link>
-    </>
+    </div>
   );
 }
 
